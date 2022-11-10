8b9e359a6d0f223dc89df0ba48dbe1b698249d3b
cirsjulyancolesco <crisjulyancolesco@iskolarngbayan.pup.edu.ph>
2022-11-10 16:15:32 +0800

    Making the Add Contact choice

--- AddressBook.py
+++ AddressBook.py
@@ -4,3 +4,11 @@ Contact = []
 while True:
     Choice = int(input("1. Add Contact \n 2. Edit Contact \n 3. Delete Contact \n 4. View Contacts \n 5. Search Address Book \n 6. Exit \n Enter your Choice: "))
 
+    # Add contact
+    if Choice == 1:
+        FirstName = input("First name: ")
+        LastName = input("Last name: ")
+        Address = input("Address: ")
+        ContactNumber = int(input("Contact Number: "))
+        NewContact = [FirstName, LastName, Address, ContactNumber]
+        Contact.append(NewContact)
