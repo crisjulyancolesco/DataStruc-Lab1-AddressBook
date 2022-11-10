0b411eb8b2fe55dc024ed89bac7a1d68576f3e1e
cirsjulyancolesco <crisjulyancolesco@iskolarngbayan.pup.edu.ph>
2022-11-10 16:11:56 +0800

    Making list and choices which the user can choose

---
+++ AddressBook.py
@@ -0,0 +1,6 @@
+Contact = []
+
+# Display the choices
+while True:
+    Choice = int(input("1. Add Contact \n 2. Edit Contact \n 3. Delete Contact \n 4. View Contacts \n 5. Search Address Book \n 6. Exit \n Enter your Choice: "))
+
