6c380b0a3697a3eed865b78e19a08e713b6f1798
crisjulyancolesco <crisjulyancolesco@iskolarngbayan.pup.edu.ph>
2022-11-11 14:48:03 +0800

    Running python program

--- AddressBook.py
+++ AddressBook.py
@@ -3,19 +3,23 @@ Contact = []
 
 # Display the choices
 while True:
-    Choice = int(input("1. Add Contact \n2. Edit Contact \n3. Delete Contact \n4. View Contacts \n5. Search Address Book \n6. Exit \nEnter your Choice: "))
-
+    print("-------------------------Address Book-------------------------")
+    print("What would you like to do?")
+    Choice = int(input("1. Add Contact \n2. Edit Contact \n3. Delete Contact \n4. View Contacts \n5. Search Address Book \n6. Exit \n\nChoose an option: "))
     # 1. Add contact
     if Choice == 1:
-        FirstName = input("First name: ")
-        LastName = input("Last name: ")
-        Address = input("Address: ")
-        ContactNumber = int(input("Contact Number: "))
+        print("\nAdd Contact")
+        FirstName = input("\tFirst name: ")
+        LastName = input("\tLast name: ")
+        Address = input("\tAddress: ")
+        ContactNumber = int(input("\tContact Number: "))
         NewContact = [FirstName, LastName, Address, ContactNumber]
         Contact.append(NewContact)
-    
+        print()
+
     # 2. Edit Contact
     elif Choice == 2:
+        print("\nEdit Contact")
         Edit = input("Enter the contact you wish to edit: ")
         New = []
         Scan = 0
@@ -24,17 +28,20 @@ while True:
                 Scan += 1
                 print(Contact[x])
                 Contact.pop(x)
-                EditedFirstName = input("First name: ")
-                EditedLastName = input("Last name: ")
-                EditedAddress = input("Address: ")
-                EditedContactNumber = int(input("Contact Number: "))
+                print("\nEdit Contact")
+                EditedFirstName = input("\tFirst name: ")
+                EditedLastName = input("\tLast name: ")
+                EditedAddress = input("\tAddress: ")
+                EditedContactNumber = int(input("\tContact Number: "))
                 EditedContact = [EditedFirstName, EditedLastName, EditedAddress, EditedContactNumber]
                 Contact.append(EditedContact)
         if Scan == 0:
-            print("The contact doesn't exist")
+            print("\nContact doesn't exist!")
+        print()
 
     # 3. Delete Contact
     elif Choice == 3:
+        print("\nDelete Contact")
         Deleted = input("Enter the contact you wish to delete: ")
         New = []
         Del = 0
@@ -43,50 +50,63 @@ while True:
                 Del += 1
                 print(Contact[x])
                 Contact.pop(x)
-                print("The contact has been deleted")
+                print("\nContact has been deleted!")
         if Del == 0:
-            print("The contact doesn't exist")
+            print("\nContact doesn't exist!")
+        print()
 
     # 4. View Contacts
     elif Choice == 4:
-        print("\tContacts\t")
+        print("\nAll Contacts")
         for x in range(len(Contact)):
                 print(Contact[x])
         if not Contact:
-            print("No Contacts")
+            print("\nNo Contacts")
+        print()
 
     # 5. Search Address Book
     elif Choice == 5:
-        print("\tCriterias to choose from\t")
-        Search = int(input("1. First Name \n2. Last Name \n3. Address \n4. Contact Number \nEnter your choice: ")) 
+        print("\nSearch Book Address")
+        print("\tCriterias to choose from:")
+        Search = int(input("\t 1. First Name \n\t 2. Last Name \n\t 3. Address \n\t 4. Contact Number \n\n\tChoose criteria: ")) 
         if Search == 1:
-            SearchFirstName = input("First Name: ")
+            SearchFirstName = input("\t First Name: ")
             for a in range(len(Contact)):
                 if SearchFirstName == Contact[a][0]:
                     print(Contact[a])
+            if not Contact:
+                print("\nContact doesn't exist!")
         if Search == 2:
-            SearchLastName = input("Last Name: ")
+            SearchLastName = input("\t Last Name: ")
             for b in range(len(Contact)):
                 if SearchLastName == Contact[b][1]:
                     print(Contact[b])
+            if not Contact:
+                print("\nContact doesn't exist!")
         if Search == 3:
-            SearchAddress = input("Address: ")
+            SearchAddress = input("\t Address: ")
             for c in range(len(Contact)):
                 if SearchAddress == Contact[c][2]:
                     print(Contact[c])
+            if not Contact:
+                print("\nContact doesn't exist!")
         if Search == 4:
-            SearchContactNumber = int(input("Contact Number: "))
+            SearchContactNumber = int(input("\t Contact Number: "))
             for d in range(len(Contact)):
                 if SearchContactNumber == Contact[d][3]:
                     print(Contact[d])
+            if not Contact:
+                print("\nContact doesn't exist!")
         if Search >=5:
-            print("Invalid Search Criteria")
+            print("\nInvalid Search Criteria!")
+        print()
 
     # 6. Exit
     elif Choice >=6:
-        Exit = input("Would you like to exit Address Book? y/n: ")
+        print("\nExit")
+        Exit = str(input("Would you like to exit Address Book? y/n: "))
         if (Exit ==' ' or Exit =='n' or Exit =='N' or Exit =='No' or Exit =='NO' or  Exit =='no'):
+            print()
             continue
-    if (Exit =='' or Exit =='Y' or Exit =='y' or Exit =='yes' or Exit =='YES' or Exit =='Yes'):
-        break
-    
+        if (Exit =='' or Exit =='Y' or Exit =='y' or Exit =='yes' or Exit =='YES' or Exit =='Yes'):
+            break
